0ab3b0314173c530994187277a4c665e21e5db81
dd-han <han@dd-han.tw>
2017-09-07 18:58:06 +0800

    update code

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def index():
 
 @app.route("/api/harekaze/ship.json")
 def api_ship():
+    # 進入 Request 後，發出一個 Request
     request = requests.get('https://api.harekaze.moe/ship.json')
+
+    # 把結果丟回去
     return request.text
+
+    ## 思考：如果 API 有很多路徑，你要怎麼處裡？可能會無法窮舉喔！
